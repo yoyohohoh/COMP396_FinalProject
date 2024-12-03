4d010547376580bb03c6d1a79767773f823f4c1d
Yiu Yiu Yoyo Ho <yho45@my.centennialcollege.ca>
2024-12-02 23:05:28 -0500

    fix medium resetposition, added result badge

--- Assets/_Scripts/UI/ResultDisplay.cs
+++ Assets/_Scripts/UI/ResultDisplay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
@@ -18,6 +19,17 @@ public class ResultDisplay : MonoBehaviour
     List<string> mediumLevelRecord;
     List<string> hardLevelRecord;
 
+    [SerializeField] Image P1Badge;
+    [SerializeField] Image P2Badge;
+    [SerializeField] Image P1EasyBadge;
+    [SerializeField] Image P2EasyBadge;
+    [SerializeField] Image P1MediumBadge;
+    [SerializeField] Image P2MediumBadge;
+    [SerializeField] Image P1HardBadge;
+    [SerializeField] Image P2HardBadge;
+
+    int p1CountWin = 0;
+    int p2CountWin = 0;
     DataKeeper dataKeeper;
     // Start is called before the first frame update
     void Start()
@@ -31,6 +43,26 @@ public class ResultDisplay : MonoBehaviour
         UpdateTimerTexts(dataKeeper.easyLevelRecord, p1EasyTimer, p2EasyTimer);
         UpdateTimerTexts(dataKeeper.mediumLevelRecord, p1MediumTimer, p2MediumTimer);
         UpdateTimerTexts(dataKeeper.hardLevelRecord, p1HardTimer, p2HardTimer);
+
+        UpdateBadges(p1EasyTimer, p2EasyTimer, P1EasyBadge, P2EasyBadge);
+        UpdateBadges(p1MediumTimer, p2MediumTimer, P1MediumBadge, P2MediumBadge);
+        UpdateBadges(p1HardTimer, p2HardTimer, P1HardBadge, P2HardBadge);
+
+        if (p1CountWin > p2CountWin)
+        {
+            P1Badge.enabled = true;
+            P2Badge.enabled = false;
+        }
+        if (p1CountWin < p2CountWin)
+        {
+            P1Badge.enabled = false;
+            P2Badge.enabled = true;
+        }
+        if (p1CountWin == p2CountWin)
+        {
+            P1Badge.enabled = true;
+            P2Badge.enabled = true;
+        }
     }
 
     void UpdateTimerTexts(List<string> levelRecord, Text p1Timer, Text p2Timer)
@@ -45,5 +77,60 @@ public class ResultDisplay : MonoBehaviour
             p2Timer.text = levelRecord[1];
             p1Timer.text = levelRecord[3];
         }
+    }
+
+    void UpdateBadges(Text p1Timer, Text p2Timer, Image p1Badge, Image p2Badge)
+    {
+
+
+        if (p1Timer.text == "Dead" && p2Timer.text == "Dead")
+        {
+            p1Badge.enabled = false;
+            p2Badge.enabled = false;
+        }
+        else if (p1Timer.text != "Dead" && p2Timer.text != "Dead")
+        {
+            TimeSpan p1Time = TimeSpan.Parse(p1Timer.text);
+            TimeSpan p2Time = TimeSpan.Parse(p2Timer.text);
+
+            if (p1Time < p2Time)
+            {
+                p1Badge.enabled = true;
+                p2Badge.enabled = false;
+                p1CountWin++;
+            }
+
+            if (p1Time > p2Time)
+            {
+                p2Badge.enabled = true;
+                p1Badge.enabled = false;
+                p2CountWin++;
+            }
+
+            if (p1Time == p2Time)
+            {
+                p1Badge.enabled = true;
+                p2Badge.enabled = true;
+                p1CountWin++;
+                p2CountWin++;
+            }
+        }
+        else
+        {
+            if (p1Timer.text == "Dead" && p2Timer.text != "Dead")
+            {
+                p2Badge.enabled = true;
+                p1Badge.enabled = false;
+                p2CountWin++;
+            }
+
+            if (p1Timer.text != "Dead" && p2Timer.text == "Dead")
+            {
+                p1Badge.enabled = true;
+                p2Badge.enabled = false;
+                p1CountWin++;
+            }
+        }
+
     }
 }
